7373daa4b7b508f27b6f8134b94fe7825079c106
ODM <danmusad@gmail.com>
2020-10-10 11:06:23 -0500

    Initial Commit

---
+++ UIC_Clothing_App/UIC_Clothing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIC_Clothing_AppApp.swift
+//  UIC_Clothing_App
+//
+//  Created by ODM on 10/10/20.
+//
+
+import SwiftUI
+
+@main
+struct UIC_Clothing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
